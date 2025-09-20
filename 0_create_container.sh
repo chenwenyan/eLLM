7b9bd9fb7cbbeb75470842ed79c085b48a76e4b8
chenwenyan <1358384755@qq.com>
2025-09-20 19:21:20 +0800

    update scripts

--- 0_create_container.sh
+++ 0_create_container.sh
@@ -14,3 +14,5 @@ docker create --name vllm_hcache --gpus=all --ipc=host --net=host -it -v /nfs/da
 docker create --name vllm_org --gpus=all --ipc=host --net=host -it -v /nfs/dataset:/nfs/dataset -v /nfs/cache:/nfs/cache -v /home/wychen/.cache:/root/.cache -v /home/wychen/workspace:/root/workspace -v /root/.cache/huggingface/hub:/root/.cache/huggingface/hub vllm_lucz:latest
 
 docker create --name vllm_dynamic --gpus=all --ipc=host --net=host -it -e CUDA_DEVICE_ORDER=PCI_BUS_ID  -v /nfs/dataset:/nfs/dataset -v /nfs/cache:/nfs/cache -v /home/wychen/.cache:/root/.cache -v /home/wychen/workspace:/root/workspace -v /root/.cache/huggingface/hub:/root/.cache/huggingface/hub vllm_wychen:latest 
+
+docker create --name vllm_spec --gpus=all --ipc=host --net=host -it -e CUDA_DEVICE_ORDER=PCI_BUS_ID  -v /nfs/dataset:/nfs/dataset -v /nfs/cache:/nfs/cache -v /home/wychen/.cache:/root/.cache -v /home/wychen/workspace:/root/workspace -v /root/.cache/huggingface/hub:/root/.cache/huggingface/hub vllm_wychen:latest 
